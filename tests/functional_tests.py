a41e44c6871674c000b489f6e0d8babe91af82f0
Luciano Filho <lvgalvaofilho@gmail.com>
2023-12-31 07:26:39 -0300

    tests: adicionar test de iniciar lista e acessar depois

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,6 +1,39 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http:localhost:8000')
+class NovoVisitanteTest(unittest.TestCase):
 
-assert 'The install worked successfully! Congratulations!' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+    
+    def test_iniciar_uma_lista_e_acessar_depois(self):
+        # Acessar a homepage
+        self.browser.get('http:localhost:8000')
+
+        # Percebe que o título da página e o cabeçalho mrnciam lista de tarefas
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('O título do browser foi: ' + self.browser.title)
+
+        # Insere um liitem de tarefa
+
+        # Ela digita 'Comprar penas de pavão' em uma caixa de texto
+
+        # Quando digita enter, a página é atualizada, e agora a pagina lista
+        # '1: Comprar penas de pavão' como um interm da lista de tarefas
+
+        # Ela digita 'Usar a penas de pavão para pescar
+
+        # Aperta enter, e a página é atualizada novamente, agora exibindo os 2 itens
+
+        # Ela anota uma URL único com a lista de tarefa
+
+        # Ela testa a URL única
+
+        # Ela anota e saí da aplicação
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
