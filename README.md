25f7889947ce21e471071f2426c02c5b54240455
Luciano Filho <lvgalvaofilho@gmail.com>
2024-01-01 06:28:01 -0300

    feat: adicionar testes de inserir itens

--- README.md
+++ README.md
@@ -10,6 +10,42 @@ O TDD é como ter uma chave-catraca que permite interromper o seu professo, faze
 
 O TDD é uma disciplina, e isso significa que não é algo que surge naturalmente; como muitas das compensações não são imediatas, mas só aparecem no longo prazo, você precisará se forçar a segui-lo no momento. 
 
+## Como executar a aplicação
+
+Utilizamos o Django para construir uma aplicação de 'To-do'
+
+1) Clonar o repositório
+```bash
+git clone tdd-com-python
+cd tdd-com-python
+```
+
+2) Configurar ambiente virtual com versão do Python e bibliotecas do projeto
+```bash
+pyenv install 3.6.15
+pyven use 3.6.15
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+
+3) Para executar a aplicação utilizar o comando
+```bash
+python superlists/manage.py runserver
+```
+
+## Como testar a aplicação
+
+### Teste funcional
+
+Nesse projeto utilizamos o Selenium para testar interações com o usuário
+
+Para executar o testes utilizar o comando
+
+```bash
+python tests/functional_tests.py
+```
+
 ## Requisitos do projeto
 
 - Firefox
--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NovoVisitanteTest(unittest.TestCase):
@@ -13,19 +15,36 @@ class NovoVisitanteTest(unittest.TestCase):
         # Acessar a homepage
         self.browser.get('http:localhost:8000')
 
-        # Percebe que o título da página e o cabeçalho mrnciam lista de tarefas
+        # Percebe que o título da página e o cabeçalho mencionam lista de tarefas
 
-        self.assertIn('To-Do', self.browser.title)
-        self.fail('O título do browser foi: ' + self.browser.title)
+        self.assertIn('To-Do lists', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-        # Insere um liitem de tarefa
+        # Insere um item de tarefa
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # Ela digita 'Comprar penas de pavão' em uma caixa de texto
+        inputbox.send_keys('Comprar penas de pavão')
 
         # Quando digita enter, a página é atualizada, e agora a pagina lista
         # '1: Comprar penas de pavão' como um interm da lista de tarefas
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_id('tr')
+        self.assertTrue(
+            any(row.text == '1: Comprar penas de pavão' for row in rows)
+        )
 
         # Ela digita 'Usar a penas de pavão para pescar
+
+        self.fail('Finalizar o testes')
 
         # Aperta enter, e a página é atualizada novamente, agora exibindo os 2 itens
 
