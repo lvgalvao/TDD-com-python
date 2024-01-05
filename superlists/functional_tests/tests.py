a530ee1f9820da490d3f1e8943a734c67f3197e5
Luciano Filho <lvgalvaofilho@gmail.com>
2024-01-05 05:44:09 -0300

    feat: adicionar testes para espera do carregamento da lista

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 from django.test import LiveServerTestCase
+from selenium.common.exceptions import WebDriverException
 
+MAX_WAIT = 10
 class NovoVisitanteTest(LiveServerTestCase):
 
     def setUp(self):
@@ -12,15 +13,25 @@ class NovoVisitanteTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
+    def wait_for_row_in_list_table(self, row_text):
         """ 
         Objetivo desse teste é trabalhar com o princípio de DRY (Don't Repeat Yourself)
         Háviamos 2 testes semelhantes para testar itens em uma lista
         Com esse teste removemos essa lógica duplicada e simpliciamos nossa lógica
         """
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep
+
+
     
     def test_iniciar_uma_lista_e_acessar_depois(self):
         # Acessar a homepage
@@ -45,19 +56,16 @@ class NovoVisitanteTest(LiveServerTestCase):
         # Quando digita enter, a página é atualizada, e agora a pagina lista
         # '1: Comprar penas de pavão' como um interm da lista de tarefas
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
-        self.check_for_row_in_list_table('1: Comprar penas de pavão')
+        self.wait_for_row_in_list_table('1: Comprar penas de pavão')
 
         # Ela digita 'Usar a penas de pavão para pescar
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Usar penas de pavão para fazer uma isca')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_for_row_in_list_table('1: Comprar penas de pavão')
-        self.check_for_row_in_list_table('2: Usar penas de pavão para fazer uma isca')
+        self.wait_for_row_in_list_table('1: Comprar penas de pavão')
+        self.wait_for_row_in_list_table('2: Usar penas de pavão para fazer uma isca')
         
         self.fail('Finalizar o testes')
 
